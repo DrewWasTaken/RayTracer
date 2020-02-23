af94d81b28fb4e6cd34bc2e04f30bd8f29e3ad2b
Drew <Drew2.Mayhew@live.uwe.ac.uk>
2020-02-23 23:32:54 +0000

    4 Walls (Inverse Flipped) + Light Added

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -34,6 +34,26 @@ vec3 color(const ray& r, hittable* world, int depth) //Blends White + Blue, Depe
 }
 
 
+hittable* cornell_box()
+{
+    hittable** list = new hittable * [6];
+    int i = 0;
+    material* red = new lambertian(new constant_texture(vec3(0.65, 0.05, 0.05)));
+    material* white = new lambertian(new constant_texture(vec3(0.73, 0.73, 0.73)));
+    material* green = new lambertian(new constant_texture(vec3(0.12, 0.45, 0.15)));
+    material* light = new diffuse_light(new constant_texture(vec3(15, 15, 15)));
+
+    list[i++] = new flip_normals(new yz_rect(0, 555, 0, 555, 555, green));
+    list[i++] = new yz_rect(0, 555, 0, 555, 0, red);
+    list[i++] = new xz_rect(213, 343, 227, 332, 554, light);
+    list[i++] = new flip_normals(new xz_rect(0, 555, 0, 555, 555, white));
+    list[i++] = new xz_rect(0, 555, 0, 555, 0, white);
+    list[i++] = new flip_normals(new xy_rect(0, 555, 0, 555, 555, white));
+
+    return new hittable_list(list, i);
+}
+
+
 hittable* simple_light()
 {
     texture* checker = new checker_texture(
@@ -157,15 +177,18 @@ int main()
 
     //hittable* world = two_spheres();
 
-    hittable* world = simple_light();
+    //hittable* world = simple_light();
+
+    hittable* world = cornell_box();
 
     //Adjust Camera Viewpoint
-    vec3 lookfrom(13, 2, 3);
-    vec3 lookat(0, 0, 0);
+    vec3 lookfrom(278, 278, -800);
+    vec3 lookat(278, 278, 0);
     float dist_to_focus = 10.0;
     float aperture = 0.0;
+    float vfov = 40.0;
 
-    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus, 0.0, 1.0);
+    camera cam(lookfrom, lookat, vec3(0,1,0), vfov, float(nx)/float(ny), aperture, dist_to_focus, 0.0, 1.0);
 
     for (int j = ny - 1; j >= 0; j--)
     {
