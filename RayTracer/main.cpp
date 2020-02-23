5f0495c7010c3dffb42bd3840b18415429048774
Drew <Drew2.Mayhew@live.uwe.ac.uk>
2020-02-23 22:42:35 +0000

    Simple Lighting Added

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -3,39 +3,50 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
-#include "sphere.h"
-#include "hittable_list.h"
-#include "float.h"
+
+#include "aarect.h"
 #include "camera.h"
-#include "numgen.h"
+#include "float.h"
+#include "hittable_list.h"
 #include "material.h"
 #include "moving_sphere.h"
+#include "numgen.h"
+#include "sphere.h"
 
 
 using namespace std;
 
-vec3 color(const ray &r, hittable *world, int depth) //Blends White + Blue, Dependant On Y Axis
+vec3 color(const ray& r, hittable* world, int depth) //Blends White + Blue, Dependant On Y Axis
 {
     hit_record rec;
     if (world->hit(r, 0.001, FLT_MAX, rec)) //0.001 Cleans Up Near 0 Hits
     {
         ray scattered;
         vec3 attenuation;
+        vec3 emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
         if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-        {
             return attenuation * color(scattered, world, depth + 1);
-        }
         else
-        {
-            return vec3(0, 0, 0);
-        }
+            return emitted;
     }
     else
-    {
-        vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
-    }
+        return vec3(0, 0, 0);
+}
+
+
+hittable* simple_light()
+{
+    texture* checker = new checker_texture(
+        new constant_texture(vec3(0.2, 0.3, 0.1)),
+        new constant_texture(vec3(0.9, 0.9, 0.9))
+    );
+    //color* pertext = new noise_texture(4);
+    hittable** list = new hittable * [4];
+    list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(checker));
+    list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(checker));
+    list[2] = new sphere(vec3(0, 7, 0), 2, new diffuse_light(new constant_texture(vec3(4, 4, 4))));
+    list[3] = new xy_rect(3, 5, 1, 3, -2, new diffuse_light(new constant_texture(vec3(4, 4, 4))));
+    return new hittable_list(list, 4);
 }
 
 hittable* two_spheres()
@@ -144,8 +155,9 @@ int main()
     
     //hittable* world = random_scene();
 
-    hittable* world = two_spheres();
+    //hittable* world = two_spheres();
 
+    hittable* world = simple_light();
 
     //Adjust Camera Viewpoint
     vec3 lookfrom(13, 2, 3);
